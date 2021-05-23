38e1f91175f3ea8b1147290a124bdd99ea14e4d2
YVodzich <yuri.vodich@gmail.com>
2021-05-23 23:01:22 +0300

    fix logical common errors

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 var express = require('express');
 var app = express();
-var db = require('./db');
+var db = require('./db').sequelize;
 var user = require('./controllers/usercontroller');
 var game = require('./controllers/gamecontroller')
+const authorization = require('./middleware/validate-session');
+const { PORT } = require('./config');
 
+app.use('/', (req, res, next) => {
+    if (req.originalUrl === '/') {
+        res.send('Service is running!');
+        return;
+    }
+    next();
+});
 
-db.sync();
-app.use(require('body-parser'));
+app.use(express.json());
+
 app.use('/api/auth', user);
-app.use(require('./middleware/validate-session'))
+app.use(authorization.checkAuthorization);
 app.use('/api/game', game);
-app.listen(function() {
+
+app.listen(PORT, () => {
     console.log("App is listening on 4000");
+    db.sync({ force: true });
 })
